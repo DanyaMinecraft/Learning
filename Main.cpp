6963ad87e30382cf90352f3d00b4f0396cea79b9
Даниил Гаврин < @COMPUTER>
2022-01-25 20:49:16 +0300

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+﻿#include "Main.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a;
+	cout << "Введите первое число: \n";
+	cin >> a;
+
+	float b;
+	cout << "Введите второе число: \n";
+	cin >> b;
+	std::system("cls");
+
+	if ( a > b ) {
+		cout << "Да, " << "число " << a << " больше числа " << b << endl;
+	}
+	else
+	{
+		cout << "Нет, " << "число " << a << " меньше числа " << b << endl;
+	}
+	cout << "I " << "love " << "the sun " << "named " << "Nastya" << endl;
+	
+	system("pause");
+
+	return 0;
+}
